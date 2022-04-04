b4372e93646e925472dad37c075918c430c8dd09
JoyTriza <wangarijoytriza@gmail.com>
2022-04-04 09:24:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Avorio", "Briannita","Ali"];
+function writeCards(names, event="birthday") {
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+
+function countDown(startingNumber) {
+    while (startingNumber > 0) {
+        console.log(startingNumber);
+        startingNumber -= 1;
+    }
+    console.log(startingNumber)
+};
